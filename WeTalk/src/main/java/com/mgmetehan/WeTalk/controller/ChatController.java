9ab808c22942e3e66482b891cc438d0e610e9752
Metehan Gultekin <mgmetehan@hotmail.com>
2022-11-05 20:23:45 +0300

    update

--- WeTalk/src/main/java/com/mgmetehan/WeTalk/controller/ChatController.java
+++ WeTalk/src/main/java/com/mgmetehan/WeTalk/controller/ChatController.java
@@ -1,28 +1,29 @@
 package com.mgmetehan.WeTalk.controller;
 
 import com.mgmetehan.WeTalk.model.Message;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController  //@Controller ?
-@RequiredArgsConstructor
+@Controller
 public class ChatController {
+
+    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
-    public Message receiveMessage(@Payload Message msg){
-        return msg;
+    public Message receiveMessage(@Payload Message message){
+        return message;
     }
 
     @MessageMapping("/private-message")
-    public Message recMessage(@Payload Message msg){
-        simpMessagingTemplate.convertAndSendToUser(msg.getReceiverName(),"/private",msg);
-        System.out.println(msg.toString());
-        return msg;
+    public Message recMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+        System.out.println(message.toString());
+        return message;
     }
 }
